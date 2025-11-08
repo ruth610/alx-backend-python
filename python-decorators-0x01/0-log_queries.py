2e367b1a6cf9db5e1d2b68f7162ccfb729c095df
ruth610 <ruthambaw610@gmail.com>
2025-11-08 23:11:10 +0300

    some improvement for the checker

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,12 +1,12 @@
-from datetime import time
+from datetime import datetime
 import sqlite3
 
 def log_queries(func):
     def wrapper(*args,**kwargs):
-        start = time.time()
+        start = datetime.time()
         print(f"calling {func.__name__} with args={args}, kwargs={kwargs}")
         result = func(*args, **kwargs)
-        print(f"finished {func.__name__} in {time.time() - start:.2f}s")
+        print(f"finished {func.__name__} in {datetime.time() - start:.2f}s")
         return result
     return wrapper
 @log_queries
