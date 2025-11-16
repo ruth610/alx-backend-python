0bde0211f805cb669627d0e58c438d3e8999c4a0
ruth610 <ruthambaw610@gmail.com>
2025-11-16 10:53:44 +0300

    exception test is not raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self,nested_map,path,expected_value):
         self.assertEqual(access_nested_map(nested_map,path),expected_value)
 
+    @parameterized.expand([
+        ({},("a",)),
+        ({"a": 1},("a", "b"))
+    ])
+    def test_access_nested_map_exception(self,nested_map,path):
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map,path)
+        self.assertEqual(str(error.exception),repr(path[-1]))
+
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -2,6 +2,8 @@ def access_nested_map(nested_map,path):
     value = nested_map
 
     for key in path:
+        if not isinstance(value,dict):
+            raise KeyError(key)
         value = value[key]
     
     return value
