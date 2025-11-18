a998516f0bdd56363060d3ea5f737e15e96cc31e
ruth610 <ruthambaw610@gmail.com>
2025-11-18 10:45:10 +0300

    resolve the routing problem

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,12 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from rest_framework import routers
 from .views import ConversationViewSet,MessageViewSet
 
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('conversations', ConversationViewSet, basename='conversations')
 router.register('messages', MessageViewSet, basename='messages')
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls))
+]
