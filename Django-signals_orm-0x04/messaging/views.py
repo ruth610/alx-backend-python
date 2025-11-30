ec3f8200d2b8fa201a0e46273e6228ffca53f0e9
Ruth Ambaw <ruthambaw610@gmail.com>
2025-11-30 17:24:39 +0300

    Signals for Deleting User-Related Data

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    """
+    Allows a user to delete their account.
+    """
+    user = request.user
+    logout(request)        # Log out first
+    user.delete()          # Trigger deletion (signals included)
+    return redirect('/')   # Redirect to home page
+
--- Django-signals_orm-0x04/messaging_app/urls.py
+++ Django-signals_orm-0x04/messaging_app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('delete-account/', delete_user, name='delete_user'),
 ]
